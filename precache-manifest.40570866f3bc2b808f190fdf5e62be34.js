d392fc2908e19bcd13e8cba58f1cd9d82c912201
nojanxoo <nojanxoo@gmail.com>
2020-09-09 18:01:59 +0900

    Updates

---
+++ precache-manifest.40570866f3bc2b808f190fdf5e62be34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab60f0b45917e58642bde20ede2bb148",
+    "url": "/2020_movie/index.html"
+  },
+  {
+    "revision": "fe6735b68497313df2a8",
+    "url": "/2020_movie/static/css/main.316b594f.chunk.css"
+  },
+  {
+    "revision": "512d170b028e0595b66a",
+    "url": "/2020_movie/static/js/2.fd92df72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/2020_movie/static/js/2.fd92df72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6735b68497313df2a8",
+    "url": "/2020_movie/static/js/main.0fa1d63e.chunk.js"
+  },
+  {
+    "revision": "c419d81020a054deaf52",
+    "url": "/2020_movie/static/js/runtime-main.f1451f1f.js"
+  }
+]);
